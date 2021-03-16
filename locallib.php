0fb6fba10ccfb5c109226556c709ba17d76890c3
Anay <anay.infowind@gmail.com>
2021-03-16 17:50:30 +0530

    Removed data if wrong license

--- locallib.php
+++ locallib.php
@@ -52,6 +52,7 @@ function updateconfclients() {
     if ($infoleeloolxp->status != 'false') {
         $leeloolxpurl = $infoleeloolxp->data->install_url;
     } else {
+        set_config('settingsjson', base64_encode($output), 'block_tb_clients');
         return;
     }
     $url = $leeloolxpurl . '/admin/Theme_setup/get_trustus_settings';

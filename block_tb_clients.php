54340c17f1b202bbcdb8f334271d888f658068ad
Anay <anay.infowind@gmail.com>
2021-03-24 14:20:38 +0530

    add owl css in styles.css

--- block_tb_clients.php
+++ block_tb_clients.php
@@ -85,8 +85,6 @@ class block_tb_clients extends block_base {
             $this->page->requires->js(new moodle_url($CFG->wwwroot . '/blocks/tb_clients/js/owlslider.js'));
         }
 
-        $this->page->requires->css(new moodle_url($CFG->wwwroot . '/blocks/tb_clients/css/owl.carousel.min.css'));
-        $this->page->requires->css(new moodle_url($CFG->wwwroot . '/blocks/tb_clients/css/owl.theme.default.min.css'));
 
         $this->content = new stdClass();
         $this->content->text = '<div class="tb_clients owl-carousel owl-theme">';

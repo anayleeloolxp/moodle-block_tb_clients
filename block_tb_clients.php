71ec865a10b2bacf1051620daa22a0314bb905fa
Anay Infowind <anay.infowind@gmail.com>
2021-04-13 12:01:28 +0530

    removed cron and syncleeloo.php

--- block_tb_clients.php
+++ block_tb_clients.php
@@ -122,12 +122,4 @@ class block_tb_clients extends block_base {
     public function applicable_formats() {
         return array('all' => true);
     }
-
-    /**
-     * Get settings from Leeloo
-     */
-    public function cron() {
-        require_once($CFG->dirroot . '/blocks/tb_clients/lib.php');
-        updateconfclients();
-    }
 }
